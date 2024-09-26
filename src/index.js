388b95386810bbbfc17ede3497d16318b4ceb82d
菜菜 <709147950@qq.com>
2024-09-26 09:10:37 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import Zhiwuxingqiu from './zhiwuxingqiu/index.js';
-import Airboshi from './airboshi/index.js'
-
-
 
 try {
     await Zhiwuxingqiu();
-    await Airboshi();
     process.exit(0);
 } catch (error) {
     process.exit(1);
